60227e56a6858ca45e746b0749570efdeb7b75f3
samuelsit <samsitruk@gmail.com>
2019-07-15 15:19:15 +0200

    98 errors

--- srcs/utils/ft_putnbr_uns_printf.c
+++ srcs/utils/ft_putnbr_uns_printf.c
@@ -7,12 +7,6 @@ int ft_putnbr_uns_printf(unsigned int nb, int display)
 
 	len = 0;
 	res = 0;
-	if (nb < 0)
-	{
-		res = 4294967296 + nb;
-		ft_putnbr_uns_printf(res, display);
-		return (0);
-	}
 	if (nb / 10)
 		ft_putnbr_uns_printf(nb / 10, display);
 	len++;
