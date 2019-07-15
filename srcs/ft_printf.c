7df786d92b3485e11ed3151e74727d1dafaf7552
samuelsit <samsitruk@gmail.com>
2019-07-15 07:56:08 +0200

    clean

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -13,122 +13,141 @@ int		putspace(int nb)
 	return (i);
 }
 
-int		ft_printf(const char *str, ...)
+void	if_nomod(const char *str, int *i, int *len)
 {
+	if (str[*i] != '%')
+	{
+		ft_putchar(str[*i]);
+		(*len)++;
+		(*i)++;
+	}
+}
+
+void	if_mod(const char *str, int *i, int *len, va_list ap)
+{
+	int n;
+	va_list aq;
+	int	(*tab_ft[NB_OPTIONS])(va_list ap, int display);
 	int nbspace;
 	int lenmod;
-	int	n;
-	int len;
-	int	i;
-	int	(*tab_ft[NB_OPTIONS])(va_list ap, int display);
-	va_list	ap;
-	va_list aq;
 
-	va_start(ap, str);
-	n = 0;
 	lenmod = 0;
 	nbspace = 0;
-	len = 0;
-	i = 0;
 	tab(tab_ft);
-	while (str[i])
+	n = 0;
+	if (str[*i] == '%')
 	{
-		if (str[i] != '%')
+		(*i)++;
+		while (str[*i] == 32)
+			(*i)++;
+		if (str[*i] == '%')
 		{
-			ft_putchar(str[i]);
-			len++;
-			i++;
+			ft_putchar('%');
+			(*len)++;
+			(*i)++;
 		}
-		if (str[i] == '%')
+		else if (str[*i] == 'l' && str[*i + 1] > 32)
 		{
-			i++;
-			while (str[i] == 32)
-				i++;
-			if (str[i] == '%')
+			n = tri_long_ft(str[*i + 1]);
+			*len += tab_ft[n](ap, 1);
+			*i += 2;
+		}
+		else if ((str[*i] >= 'a' && str[*i] <= 'z') || (str[*i] >= 'A' && str[*i] <= 'Z'))
+		{
+			n = tri_ft(str[*i]);
+			*len += tab_ft[n](ap, 1);
+			(*i)++;
+		}
+		else if (str[*i] >= '0' && str[*i] <= '9')
+		{
+			nbspace = ft_atoi_printf(&str[*i]);
+			*len += nbspace;
+			while (str[*i] >= '0' && str[*i] <= '9')
+				(*i)++;
+			if ((str[*i] >= 'a' && str[*i] <= 'z') || (str[*i] >= 'A' && str[*i] <= 'Z'))
+			{
+				n = tri_ft(str[*i]);
+				va_copy(aq, ap);
+				lenmod = tab_ft[n](ap, 0);
+				putspace(nbspace - lenmod);
+				lenmod = tab_ft[n](aq, 1);
+			}
+			if (str[*i] == '%')
+			{
+				putspace(nbspace - 1);
+				ft_putchar('%');
+			}
+			(*i)++;
+		}
+		else if (str[*i] == '+')
+		{
+			(*i)++;
+			nbspace = ft_atoi_printf(&str[*i]);
+			*len += nbspace;
+			while (str[*i] >= '0' && str[*i] <= '9')
+				(*i)++;
+			if ((str[*i] >= 'a' && str[*i] <= 'z') || (str[*i] >= 'A' && str[*i] <= 'Z'))
+			{
+				n = tri_ft(str[*i]);
+				va_copy(aq, ap);
+				lenmod = tab_ft[n](ap, 0) + 1;
+				putspace(nbspace - lenmod);
+				ft_putchar('+');
+				lenmod = tab_ft[n](aq, 1);
+			}
+			if (str[*i] == '%')
+			{
+				putspace(nbspace - 1);
+				ft_putchar('%');
+			}
+			(*i)++;
+		}
+		else if (str[*i] == '-')
+		{
+			(*i)++;
+			nbspace = ft_atoi_printf(&str[*i]);
+			*len += nbspace;
+			while (str[*i] >= '0' && str[*i] <= '9')
+				(*i)++;
+			if ((str[*i] >= 'a' && str[*i] <= 'z') || (str[*i] >= 'A' && str[*i] <= 'Z'))
+			{
+				n = tri_ft(str[*i]);
+				lenmod = tab_ft[n](ap, 1);
+				putspace(nbspace - lenmod);
+			}
+			if (str[*i] == '%')
 			{
 				ft_putchar('%');
-				len++;
-				i++;
+				putspace(nbspace - 1);
 			}
-			else if (str[i] == 'l' && str[i + 1] > 32)
-			{
-				n = tri_long_ft(str[i + 1]);
-				len += tab_ft[n](ap, 1);
-				i += 2;
-			}
-			else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-			{
-				n = tri_ft(str[i]);
-				len += tab_ft[n](ap, 1);
-				i++;
-			}
-			else if (str[i] >= '0' && str[i] <= '9')
-			{
-				nbspace = ft_atoi_printf(&str[i]);
-				len += nbspace;
-				while (str[i] >= '0' && str[i] <= '9')
-					i++;
-				if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-				{
-					n = tri_ft(str[i]);
-					va_copy(aq, ap);
-					lenmod = tab_ft[n](ap, 0);
-					putspace(nbspace - lenmod);
-					lenmod = tab_ft[n](aq, 1);
-				}
-				if (str[i] == '%')
-				{
-					putspace(nbspace - 1);
-					ft_putchar('%');
-				}
-				i++;
-			}
-			else if (str[i] == '+')
-			{
-				i++;
-				nbspace = ft_atoi_printf(&str[i]);
-				len += nbspace;
-				while (str[i] >= '0' && str[i] <= '9')
-					i++;
-				if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-				{
-					n = tri_ft(str[i]);
-					va_copy(aq, ap);
-					lenmod = tab_ft[n](ap, 0) + 1;
-					putspace(nbspace - lenmod);
-					ft_putchar('+');
-					lenmod = tab_ft[n](aq, 1);
-				}
-				if (str[i] == '%')
-				{
-					putspace(nbspace - 1);
-					ft_putchar('%');
-				}
-				i++;
-			}
-			else if (str[i] == '-')
-			{
-				i++;
-				nbspace = ft_atoi_printf(&str[i]);
-				len += nbspace;
-				while (str[i] >= '0' && str[i] <= '9')
-					i++;
-				if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
-				{
-					n = tri_ft(str[i]);
-					lenmod = tab_ft[n](ap, 1);
-					putspace(nbspace - lenmod);
-				}
-				if (str[i] == '%')
-				{
-					ft_putchar('%');
-					putspace(nbspace - 1);
-				}
-				i++;
-			}
+			(*i)++;
 		}
 	}
-	va_end(ap);
 	va_end(aq);
+}
+
+int		print(const char *str, va_list ap)
+{
+	int	i;
+	int len;
+
+	len = 0;
+	i = 0;
+	while (str[i])
+	{
+		if_nomod(str, &i, &len);
+		if_mod(str, &i, &len, ap);
+	}
+	return (len);
+}
+
+int		ft_printf(const char *str, ...)
+{
+	int len;
+	va_list	ap;
+
+	va_start(ap, str);
+	len = print(str, ap);
+	va_end(ap);
 	return (len);
 }
